c974d3fd07ed7fe2e49414df1d5d332797dd446b
Eric Schanet <eric.schanet@cern.ch>
2020-10-20 15:06:12 +0200

    script to produce simplified patch file

---
+++ makeSimplifiedPatch.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import click
+import pathlib
+import subprocess
+import re
+import json
+import numpy as np
+
+import pyhf
+
+pattern = re.compile("(\d+(?:p[05])?)_(\d+(?:p[05])?)")
+
+def string_to_float(string):
+    return float(string.replace("p", "."))
+
+@click.command()
+@click.option(
+    "--group",
+    default="1Lbb",
+    type=click.Choice(["1Lbb", "2L0J", "compressed"]),
+)
+@click.option(
+    "--output-file",
+    help="The location of the output json file. If not specified, prints to screen.",
+    default=None,
+)
+def main(group, output_file):
+    ws = pyhf.Workspace(json.load(open(pathlib.Path(f"./analyses/{group}/likelihoods/BkgOnly.json"), "r")))
+    patchset = pyhf.PatchSet(json.load(open(pathlib.Path(f"./analyses/{group}/likelihoods/patchset.json"), "r")))
+
+    simplified_patchset = {}
+    simplified_patchset['metadata'] = patchset.metadata
+    simplified_patchset['version'] = patchset.version
+    simplified_patchset['patches'] = []
+    for patch in patchset:
+
+        simplified_patch = {}
+        simplified_patch['metadata'] = patch.metadata
+        simplified_patch['name'] = patch.name
+        simplified_patch['patch'] = []
+
+        for p in patch.patch:
+            p['value']['modifiers'] = [
+                {
+                    "data": None,
+                    "name": "lumi",
+                    "type": "lumi"
+                },
+                {
+                    "data": None,
+                    "name": "mu_Sig",
+                    "type": "normfactor"
+                }
+            ]
+            p['path'] = p['path'].split('samples/')[0]+'samples/2'
+            simplified_patch['patch'].append(
+                p
+            )
+        simplified_patchset['patches'].append(simplified_patch)
+
+    if output_file is None:
+        click.echo(json.dumps(simplified_patchset, indent=4, sort_keys=True))
+    else:
+        with open(output_file, "w+") as out_file:
+            json.dump(simplified_patchset, out_file, indent=4, sort_keys=True)
+        click.echo("Written to {0:s}".format(output_file))
+        
+        
+ 
+if __name__ == "__main__":
+    main()
